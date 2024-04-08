f8043016d7ead3df59f94a13f24883c99ca3731b
nayan1306 <vnayan1306@gmail.com>
2024-04-08 22:06:56 +0530

    pi-chart for Object  detection  final

--- lib/ui/page/analysis_page.dart
+++ lib/ui/page/analysis_page.dart
@@ -1,10 +1,11 @@
 import 'dart:developer';
 
+import 'package:bigaze/ui/page/charts/pi_chart_object.dart';
 import 'package:bigaze/widgets/purpleglass.dart';
 import 'package:flutter/material.dart';
 import 'package:bigaze/model/proctor_model.dart';
 import 'package:bigaze/ui/page/common/widget/appbar.dart';
-import 'package:bigaze/ui/page/charts/pi_chart.dart';
+import 'package:bigaze/ui/page/charts/pi_chart_audio.dart';
 
 class AnalysisReportPage extends StatelessWidget {
   final ProctorModel record;
@@ -13,19 +14,21 @@ class AnalysisReportPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    log(record.audio.toString());
+    log(record.object.toString());
     return Scaffold(
       appBar: const CommonAppBar(title: 'A N A L Y S I S'),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Pi Chart Audio
             PurpleGlassCard(
               child: PieChartAudio(
                 record: record,
               ),
             ),
-            // Pi Chart
+            // pi chart Object detection
+            PurpleGlassCard(child: PieChartObject(record: record)),
             const PurpleGlassCard(
               child: Text("Line Chart"),
             ),
---
+++ lib/ui/page/charts/pi_chart_object.dart
@@ -0,0 +1,249 @@
+import 'dart:math';
+
+import 'package:fl_chart/fl_chart.dart';
+import 'package:bigaze/model/proctor_model.dart';
+import 'package:flutter/material.dart';
+
+class PieChartObject extends StatefulWidget {
+  final ProctorModel record;
+  const PieChartObject({Key? key, required this.record}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => PieChart2State();
+}
+
+class PieChart2State extends State<PieChartObject> {
+  int touchedIndex = -1;
+  late ProctorModel record;
+
+  @override
+  void initState() {
+    super.initState();
+    record = widget.record; // Accessing the passed data
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Color> colors = generateRandomColors(record.audio);
+
+    return AspectRatio(
+      aspectRatio: 0.7,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(top: 20),
+            height: 42,
+            color: Colors.transparent,
+            child: const Center(
+              child: Text(
+                "Objects Detected",
+                style: TextStyle(
+                    color: Colors.white, fontSize: 20, letterSpacing: 5),
+              ),
+            ),
+          ),
+          AspectRatio(
+            aspectRatio: 1.5,
+            child: PieChart(
+              PieChartData(
+                pieTouchData: PieTouchData(
+                  touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                    setState(() {
+                      if (!event.isInterestedForInteractions ||
+                          pieTouchResponse == null ||
+                          pieTouchResponse.touchedSection == null) {
+                        touchedIndex = -1;
+                        return;
+                      }
+                      touchedIndex =
+                          pieTouchResponse.touchedSection!.touchedSectionIndex;
+                    });
+                  },
+                ),
+                borderData: FlBorderData(
+                  show: false,
+                ),
+                sectionsSpace: 1.0,
+                centerSpaceRadius: 40,
+                sections: showingSections(record.object, colors),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 80),
+              child: ListView(
+                children: showingIndicators(record.object, colors),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  List<PieChartSectionData> showingSections(
+      List<dynamic> audioData, List<Color> colors) {
+    Map<String, double> categoryMap = {};
+
+    // Calculate the total value for each category
+    for (var item in audioData) {
+      String category = item.keys.first;
+      double value = double.parse(item.values.first.replaceAll('%', ''));
+
+      categoryMap[category] = (categoryMap[category] ?? 0) + value;
+    }
+
+    // Calculate the total sum of all categories
+    double total =
+        categoryMap.values.reduce((value, element) => value + element);
+
+    // Convert category values to percentages
+    Map<String, double> percentages = {};
+    categoryMap.forEach((category, value) {
+      percentages[category] = value / total * 100;
+    });
+
+    // Generate PieChart sections based on percentages
+    return percentages.entries.map((entry) {
+      final isTouched = entry.key == touchedIndex.toString();
+      final fontSize = isTouched ? 25.0 : 16.0;
+      final radius = isTouched ? 60.0 : 50.0;
+      const shadows = [
+        Shadow(color: Color.fromARGB(255, 27, 26, 26), blurRadius: 2)
+      ];
+
+      return PieChartSectionData(
+        color: colors[percentages.keys.toList().indexOf(entry.key)],
+        value: entry.value,
+        title: '${entry.value.toStringAsFixed(2)}%',
+        radius: radius,
+        titleStyle: TextStyle(
+          fontSize: fontSize,
+          fontWeight: FontWeight.bold,
+          color: const Color.fromARGB(255, 234, 234, 234),
+          shadows: shadows,
+        ),
+      );
+    }).toList();
+  }
+
+  List<Widget> showingIndicators(List<dynamic> audioData, List<Color> colors) {
+    Map<String, double> categoryMap = {};
+    List<Widget> indicators = [];
+
+    // Calculate the total value for each category
+    for (var item in audioData) {
+      String category = item.keys.first;
+      double value = double.parse(item.values.first.replaceAll('%', ''));
+
+      categoryMap[category] = (categoryMap[category] ?? 0) + value;
+    }
+
+    // Calculate the total sum of all categories
+    double total =
+        categoryMap.values.reduce((value, element) => value + element);
+
+    // Convert category values to percentages
+    Map<String, double> percentages = {};
+    categoryMap.forEach((category, value) {
+      percentages[category] = value / total * 100;
+    });
+
+    // Generate corresponding Indicator widgets for categories sorted by average values
+    List<MapEntry<String, double>> sortedEntries = percentages.entries.toList()
+      ..sort((a, b) {
+        return b.value.compareTo(a.value); // Sort in descending order
+      });
+
+    for (var entry in sortedEntries) {
+      indicators.add(
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Indicator(
+              color: colors[sortedEntries.indexOf(entry)],
+              text: entry.key,
+              isSquare: true,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+          ],
+        ),
+      );
+    }
+
+    return indicators;
+  }
+
+  Color getRandomColor() {
+    final Random random = Random();
+    return Color.fromRGBO(
+      random.nextInt(256), // Red value between 0 and 255
+      random.nextInt(256), // Green value between 0 and 255
+      random.nextInt(256), // Blue value between 0 and 255
+      1.0, // Alpha value (opacity) - 1.0 for fully opaque
+    );
+  }
+
+  List<Color> generateRandomColors(List<dynamic> audioData) {
+    List<Color> colors = [];
+
+    // ignore: unused_local_variable
+    for (var item in audioData) {
+      colors.add(getRandomColor());
+    }
+
+    return colors;
+  }
+}
+
+// --------------------------------------------------------------------------------
+// Utilities ======================================================================
+// --------------------------------------------------------------------------------
+class Indicator extends StatelessWidget {
+  const Indicator({
+    super.key,
+    required this.color,
+    required this.text,
+    required this.isSquare,
+    this.size = 16,
+    this.textColor,
+  });
+  final Color color;
+  final String text;
+  final bool isSquare;
+  final double size;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+            shape: isSquare ? BoxShape.rectangle : BoxShape.circle,
+            color: color,
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        Text(
+          text,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.normal,
+            color: Colors.white,
+          ),
+        )
+      ],
+    );
+  }
+}
