a7bb82de87f60cf84fcb0fa9399ae3f124416a85
amartripathi0 <theamartripathi@gmail.com>
2024-04-13 11:46:53 +0530

    updated:landing page signin signup button

--- web_frontend/src/components/shared/buttons/IndigoButton.tsx
+++ web_frontend/src/components/shared/buttons/IndigoButton.tsx
@@ -2,9 +2,9 @@ import { ButtonProps } from "@/types"
 function IndigoButton({label , handleButtonClick , additionalStyles , icon : Icon} : ButtonProps) {
   return (
     <button className={`${additionalStyles} 
-        border-white border p-4 rounded-xl text-xs font-semibold uppercase  h-full flex-between gap-2
-         bg-gradient-to-r from-purple-800  to-indigo-900 hover:border-purple-500 hover:shadow-md hover:shadow-purple-500
-         hover:scale-105 hover:transition-all duration-300
+         border-indigo-900 border p-4 rounded text-xs font-bold uppercase  h-full flex-between gap-2
+         bg-gradient-to-r from-purple-800 to-slate-900 via-indigo-900 hover:border-purple-500 hover:shadow-sm hover:shadow-purple-500
+         hover:scale-[1.02] hover:transition-all duration-300 ease-in-out
     `}
     onClick={() => handleButtonClick()}
         > 
