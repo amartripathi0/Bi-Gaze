2a8fca05127a478eac0bf96177ebd0ad48520fba
nayan1306 <vnayan1306@gmail.com>
2024-09-01 15:09:04 +0530

    Study essentials card

--- lib/ui/page/home_page.dart
+++ lib/ui/page/home_page.dart
@@ -5,6 +5,7 @@ import 'package:bigaze/object_detection/od_yolov2_tiny.dart';
 import 'package:bigaze/ui/page/common/widget/appbar.dart';
 import 'package:bigaze/ui/page/common/widget/bottomnavigationbar.dart';
 import 'package:bigaze/ui/page/proctor_page.dart';
+import 'package:bigaze/widgets/studyessentialscard.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bigaze/ui/page/profile_page.dart';
@@ -54,6 +55,10 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               SizedBox.square(
                 dimension: 20,
+              ),
+              StudyEsssentialsCard(
+                destinationPage: ProctorPage(),
+                cardName: "Study essentials card",
               ),
               GlassCard(
                 destinationPage: ProctorPage(),
---
+++ lib/widgets/studyessentialscard.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
+
+class StudyEsssentialsCard extends StatelessWidget {
+  final Widget destinationPage;
+  final String cardName;
+
+  const StudyEsssentialsCard(
+      {super.key, required this.destinationPage, required this.cardName});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => destinationPage));
+      },
+      child: GlassmorphicContainer(
+        width: 350,
+        height: 150,
+        borderRadius: 20,
+        blur: 20,
+        alignment: Alignment.center,
+        border: 2,
+        linearGradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            const Color(0xFFffffff).withOpacity(0.1),
+            const Color(0xFFFFFFFF).withOpacity(0.05),
+          ],
+          stops: const [
+            0.1,
+            1,
+          ],
+        ),
+        borderGradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            const Color(0xFFffffff).withOpacity(0.5),
+            const Color(0xFFFFFFFF).withOpacity(0.5),
+          ],
+        ),
+        child: Text(
+          cardName,
+          style: const TextStyle(
+              color: Color.fromARGB(172, 178, 172, 179), fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
