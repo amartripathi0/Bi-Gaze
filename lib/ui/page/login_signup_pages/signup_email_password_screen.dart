f39680c4658b5576dcf77c915b8aa2160792e928
nayan1306 <vnayan1306@gmail.com>
2024-09-06 15:18:05 +0530

    signup page redesigned

--- lib/ui/page/login_signup_pages/signup_email_password_screen.dart
+++ lib/ui/page/login_signup_pages/signup_email_password_screen.dart
@@ -32,14 +32,37 @@ class _EmailPasswordSignupState extends State<EmailPasswordSignup> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              "Sign Up",
-              style: TextStyle(
-                fontSize: 36,
-                color: Colors.blueAccent,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.5,
-              ),
+            Row(
+              children: [
+                Image.network(
+                  "https://raw.githubusercontent.com/nayan1306/assets/main/mail.gif",
+                  height: 100,
+                  width: 100,
+                ),
+                const Text(
+                  "Sign Up",
+                  style: TextStyle(
+                    fontSize: 36,
+                    color: Color.fromARGB(187, 197, 218, 255),
+                    letterSpacing: 1.5,
+                    shadows: [
+                      Shadow(
+                        blurRadius:
+                            5.0, // The higher the blurRadius, the softer the glow
+                        color: Color.fromARGB(
+                            255, 181, 236, 255), // Glow color (light blue)
+                        offset: Offset(0, 0), // Glow direction (centered)
+                      ),
+                      Shadow(
+                        blurRadius:
+                            20.0, // Additional shadow for more intense glow
+                        color: Color.fromARGB(255, 236, 250, 255),
+                        offset: Offset(0, 0),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: MediaQuery.of(context).size.height * 0.05),
             Container(
@@ -54,7 +77,7 @@ class _EmailPasswordSignupState extends State<EmailPasswordSignup> {
               margin: const EdgeInsets.symmetric(horizontal: 20),
               child: CustomPasswordTextField(
                 controller: passwordController,
-                hintText: 'Enter your password',
+                hintText: 'Set your password',
                 obscureText: true,
               ),
             ),
