21a8106babd721140995e91e44c9cca81b0636ea
nayan1306 <vnayan1306@gmail.com>
2024-09-06 16:02:38 +0530

    phone screen redesigned

--- lib/ui/page/login_signup_pages/phone_screen.dart
+++ lib/ui/page/login_signup_pages/phone_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:developer';
+
 import 'package:bigaze/services/firebase_auth_methods.dart';
 import 'package:bigaze/widgets/custom_button.dart';
 import 'package:bigaze/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:flutter/services.dart';
 
 class PhoneScreen extends StatefulWidget {
   static String routeName = '/phone';
@@ -14,6 +18,7 @@ class PhoneScreen extends StatefulWidget {
 
 class _PhoneScreenState extends State<PhoneScreen> {
   final TextEditingController phoneController = TextEditingController();
+  String _selectedCountryCode = '+91'; // Default country code
 
   @override
   void dispose() {
@@ -21,25 +26,82 @@ class _PhoneScreenState extends State<PhoneScreen> {
     phoneController.dispose();
   }
 
+  bool isValidPhoneNumber(String phoneNumber) {
+    // Simple regex validation for phone numbers (adjust for different countries if needed)
+    final validPhoneRegex = RegExp(r'^[0-9]{9,15}$');
+    return validPhoneRegex.hasMatch(phoneNumber);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomTextField(
-            controller: phoneController,
-            hintText: 'Enter phone number',
-          ),
-          CustomButton(
-            onTap: () {
-              context
-                  .read<FirebaseAuthMethods>()
-                  .phoneSignIn(context, phoneController.text);
-            },
-            text: 'OK',
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Phone Authentication',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 10),
+            // Country Code Picker
+            Row(
+              children: [
+                CountryCodePicker(
+                  onChanged: (code) {
+                    setState(() {
+                      _selectedCountryCode = code.dialCode!;
+                    });
+                  },
+                  initialSelection: 'IN', // Default to US
+                  favorite: const [
+                    '+91',
+                    'IN'
+                  ], // Add your preferred countries here
+                  showCountryOnly: false,
+                  showOnlyCountryWhenClosed: false,
+                  alignLeft: false,
+                  textStyle: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(width: 2),
+                Expanded(
+                  child: CustomPhoneTextField(
+                    controller: phoneController,
+                    hintText: 'Enter phone number',
+                    keyboardType: TextInputType.phone,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            CustomButton(
+              onTap: () {
+                String fullPhoneNumber =
+                    '$_selectedCountryCode${phoneController.text}';
+                log(_selectedCountryCode);
+                if (isValidPhoneNumber(phoneController.text)) {
+                  context
+                      .read<FirebaseAuthMethods>()
+                      .phoneSignIn(context, fullPhoneNumber);
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                        content: Text('Please enter a valid phone number')),
+                  );
+                }
+              },
+              text: 'Verify',
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
@@ -148,6 +149,74 @@ class _CustomPasswordTextFieldState extends State<CustomPasswordTextField> {
                     },
                   )
                 : null,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomPhoneTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  final TextInputType? keyboardType; // Add this to allow keyboard type changes
+  final List<TextInputFormatter>?
+      inputFormatters; // Add this for input formatting
+
+  const CustomPhoneTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    this.keyboardType, // Initialize the keyboard type
+    this.inputFormatters, // Initialize the input formatters
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(1.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: Colors.white, // White border color
+            width: 2, // Border width
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.cyan
+                  .withOpacity(0.6), // Cyan shadow color with opacity
+              spreadRadius: 3, // Spreads the shadow to create a glowing effect
+              blurRadius: 10, // Softens the shadow
+            ),
+          ],
+        ),
+        child: TextField(
+          controller: controller,
+          keyboardType: keyboardType, // Use the keyboard type
+          inputFormatters: inputFormatters, // Add input formatters here
+          style: const TextStyle(color: Colors.white), // White text color
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(color: Colors.transparent, width: 0),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(color: Colors.transparent, width: 0),
+            ),
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+            filled: true,
+            fillColor: const Color.fromARGB(
+                172, 0, 0, 0), // Semi-transparent background
+            hintText: hintText,
+            hintStyle: const TextStyle(
+              color: Color.fromARGB(
+                  120, 255, 255, 255), // Lighter white for hint text
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
           ),
         ),
       ),
