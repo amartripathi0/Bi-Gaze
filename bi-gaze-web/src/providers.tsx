64db34efce71e2f3c28399682437d68d4e4bd51b
amartripathi0 <theamartripathi@gmail.com>
2024-12-02 22:54:14 +0530

    add tanstack query

--- bi-gaze-web/src/providers.tsx
+++ bi-gaze-web/src/providers.tsx
@@ -1,14 +1,17 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import ReactQueryProvider from "./react-query";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <ReactQueryProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </ReactQueryProvider>
   );
 }
